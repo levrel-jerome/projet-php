b76a3667f3c0305e447f8bdee280898fc608d38e
Levrel Jerome <53918427+sheitan90@users.noreply.github.com>
2019-11-26 15:02:57 +0100

    8eme commit
    Création du fichier dev.php et index.php. retouche du fichier DAO ou l'on retire les constantes, ainsi que l'appel a self. Retouche des liens en conséquence sur home et single

--- templates/home.php
+++ templates/home.php
@@ -21,7 +21,7 @@ use App\src\DAO\ArticleDAO;
     {
         ?>
         <div>
-            <h2><a href="single.php?articleId=<?= htmlspecialchars($article->id);?>"><?= htmlspecialchars($article->title);?></a></h2>
+            <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->id);?>"><?= htmlspecialchars($article->title);?></a></h2>
             <p><?= htmlspecialchars($article->content);?></p>
             <p><?= htmlspecialchars($article->author);?></p>
             <p>Créé le : <?= htmlspecialchars($article->createdAt);?></p>
