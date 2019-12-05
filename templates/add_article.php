c4b05650ebb36ceca882874d101dc64b9a9e2de0
Levrel Jerome <53918427+sheitan90@users.noreply.github.com>
2019-12-05 14:19:33 +0100

    Création de ArticleValidation, Constraint et Validation.php
    ArticleValidation devra valider que les données d'un article soit conforme, Constraint contiendra nos contraintes de validation, Validation sera appelé par le controleur qui prendra 2 parametres: les données a valider et le nom de la classe que l'on veut valider. Modification de Controller.php ou l'on fait appel a la class Validation. Modification de add_article pour afficher les eventuels messages d'erreurs. BackController modifier en conséquence.

--- templates/add_article.php
+++ templates/add_article.php
@@ -1,7 +1,16 @@
-<?php $this->title = "Nouvel article"; ?>
-<h1>Mon blog</h1>
-<p>En construction</p>
-<div>
-    <?php include('form_article.php');?>
-    <a href="../public/index.php">Retour à l'accueil</a>
-</div>
+<?php
+$route = isset($post) && $post->get('id') ? 'editArticle&articleId='.$post->get('id') : 'addArticle';
+$submit = $route === 'addArticle' ? 'Envoyer' : 'Mettre à jour';
+?>
+<form method="post" action="../public/index.php?route=<?= $route; ?>">
+    <label for="title">Titre</label><br>
+    <input type="text" id="title" name="title" value="<?= isset($post) ? htmlspecialchars($post->get('title')): ''; ?>"><br>
+    <?= isset($errors['title']) ? $errors['title'] : ''; ?>
+    <label for="content">Contenu</label><br>
+    <textarea id="content" name="content"><?= isset($post) ? htmlspecialchars($post->get('content')): ''; ?></textarea><br>
+    <?= isset($errors['content']) ? $errors['content'] : ''; ?>
+    <label for="author">Auteur</label><br>
+    <input type="text" id="author" name="author" value="<?= isset($post) ? htmlspecialchars($post->get('author')): ''; ?>"><br>
+    <?= isset($errors['author']) ? $errors['author'] : ''; ?>
+    <input type="submit" value="<?= $submit; ?>" id="submit" name="submit">
+</form>
